52b0dcbe6d2deeba246a6d43e2dd815b36da11e9
Guz117 <cristian.guzman117@gmail.com>
2022-02-16 17:23:24 +0100

    stampa in pagina

--- index.php
+++ index.php
@@ -10,3 +10,27 @@ $user = new User('Aldo', 'Baglio', 33, 'aldo.baglio@gmail.com', new Card('Aldo b
 
 $prime = new PrimeUser("prime");
 var_dump($prime->getDiscount());
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <li><?= $user->getName() ?></li>
+        <li><?= $user->getLast_name() ?></li>
+        <li><?= $user->getEta() ?></li>
+        <li><?= $user->getMail() ?></li>
+        <li>sconto: <?= $prime->getDiscount() ?>%</li>
+    </ul>
+</body>
+
+</html>
